1974916a1a2d33c1b1b45888bae8e107c8e5e7a4
Ilia Kornilov <Ninetailedkot92@gmail.com>
2023-04-30 12:35:57 +0300

    Add enter trigger component and game exension for checking layers (#2)
    * add enter trigger component and game exension
    
    * Change collider type to 2D

---
+++ Assets/Scripts/Utils/GameObjectExtension.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class GameObjectExtension
+    {
+        public static bool IsInLayer(this GameObject go, LayerMask layer) =>
+            layer == (layer | 1 << go.layer);
+    }
+}
