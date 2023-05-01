921a339505fc8f41296a44d854d9b82bff46c47c
Fenneig <ninetailedkot92@gmail.com>
2023-05-01 16:19:22 +0300

    PlayerData

---
+++ Assets/Scripts/Model/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Model
+{
+    public class GameSession : MonoBehaviour
+    {
+        [SerializeField] private PlayerData _playerData;
+
+        public PlayerData PlayerData => _playerData;
+        
+        public static GameSession Instance;
+        
+        private void Awake()
+        {
+            Instance ??= this;
+        }
+    }
+}
