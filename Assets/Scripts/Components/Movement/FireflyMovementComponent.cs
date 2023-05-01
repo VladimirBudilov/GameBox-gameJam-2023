997e9f859604b2c536a005532534b21100b7840d
Fenneig <ninetailedkot92@gmail.com>
2023-05-01 16:07:18 +0300

    add layers, remove extra logic, fix jump

--- Assets/Scripts/Components/Movement/FireflyMovementComponent.cs
+++ Assets/Scripts/Components/Movement/FireflyMovementComponent.cs
@@ -6,13 +6,11 @@ namespace Components.Movement
     public class FireflyMovementComponent : MonoBehaviour
     {
         [Header("Settings fields")]
-        [SerializeField] private float _chaseSpeed;
         [SerializeField] private float _speed;
         [SerializeField] private int _flyToTargetDirectionDistance = 4;
         
         [Space][Header("System fields")]
         [SerializeField] private Transform _targetDirectionTransform;
-        [SerializeField] private Transform _flyToPositionWhileIdle;
 
         private Rigidbody2D _rigidbody;
         public Vector2 Direction { get; set; }
@@ -27,8 +25,6 @@ namespace Components.Movement
             CalculateVelocity();
             
             CalculateTargetDirectionHelper();
-
-            if (Direction.x == 0 && Direction.y == 0) ChasePlayer();
         }
 
         private void CalculateVelocity()
@@ -39,13 +35,6 @@ namespace Components.Movement
         private void CalculateTargetDirectionHelper()
         {
             _targetDirectionTransform.localPosition = Direction * _flyToTargetDirectionDistance;
-        }
-
-        private void ChasePlayer()
-        {
-            var directionToFly = _flyToPositionWhileIdle.position - transform.position;
-            if (directionToFly.magnitude < 1f) return;
-            _rigidbody.velocity = directionToFly.normalized * _chaseSpeed;
         }
     }
 }
--- Assets/Scripts/Components/Movement/PlayerMovementComponent.cs
+++ Assets/Scripts/Components/Movement/PlayerMovementComponent.cs
@@ -1,12 +1,18 @@
+using Components.ColliderBased;
 using UnityEngine;
 
 namespace Components.Movement
 {
     public class PlayerMovementComponent : MonoBehaviour
     {
+        [Header("Settings fields")]
         [SerializeField] private float _speed;
         [SerializeField] private float _jumpForce;
+        [Space][Header("Checkers")]
+        [SerializeField] private LayerCheck _groundCheck;
         private Rigidbody2D _rigidbody;
+        private bool _isJumping;
+        private bool _isGrounded;
 
         public float Direction { get; set; }
         public bool IsJumpPressing { get; set; }
@@ -14,6 +20,11 @@ namespace Components.Movement
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _isGrounded = _groundCheck.IsTouchingLayer;
         }
 
         private void FixedUpdate()
@@ -33,12 +44,31 @@ namespace Components.Movement
 
         private float CalculateYVelocity()
         {
-            if (!IsJumpPressing) return _rigidbody.velocity.y;
-            if (Mathf.Abs(_rigidbody.velocity.y) > 0.01f)
-                return _rigidbody.velocity.y;
-            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-            IsJumpPressing = false;
-            return _rigidbody.velocity.y;
+            var yVelocity = _rigidbody.velocity.y;
+            if (IsJumpPressing)
+            {
+                _isJumping = true;
+
+                var isFalling = yVelocity <= 0.001f;
+
+                yVelocity = isFalling ? CalculateJumpVelocity(yVelocity) : yVelocity;
+            }
+            else if (yVelocity > 0 && _isJumping)
+            {
+                yVelocity /= 2f;
+            }
+
+            return yVelocity;
+        }
+
+        private float CalculateJumpVelocity(float yVelocity)
+        {
+            if (_isGrounded)
+            {
+                yVelocity = _jumpForce;
+            }
+
+            return yVelocity;
         }
 
         private float CalculateXVelocity()
