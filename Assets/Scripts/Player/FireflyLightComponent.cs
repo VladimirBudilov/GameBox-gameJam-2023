ee8b137c3eab33444d4cc6a8f33ca40074d8daf3
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-05-02 13:08:06 +0400

    Add savezone

--- Assets/Scripts/Player/FireflyLightComponent.cs
+++ Assets/Scripts/Player/FireflyLightComponent.cs
@@ -1,6 +1,9 @@
+using System;
+using Components.ColliderBased;
 using Model;
 using PersistantData;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Utils;
 
 namespace Player
@@ -8,7 +11,11 @@ namespace Player
     public class FireflyLightComponent : MonoBehaviour
     {
         [SerializeField] private float _maxFireflyLight;
-        [SerializeField] private Timer _fireflyLoseLightTimer;
+        [SerializeField] private Timer _loseLightTimer;
+        [SerializeField] private float _lightLoseByTick;
+        [SerializeField] private Timer _regenLightTimer;
+        [SerializeField] private float _lightRegenByTick;
+        [SerializeField] private LayerCheck _saveZoneMask;
         private FloatProperty _fireflyLight;
 
         public FloatProperty FireflyLight { get => _fireflyLight; set => _fireflyLight = value; }
@@ -16,7 +23,24 @@ namespace Player
         {
             FireflyLight = GameSession.Instance.PlayerData.FireflyLight;
             FireflyLight.Value = _maxFireflyLight;
-            GameSession.Instance.PlayerData.MaxSanity = _maxFireflyLight;
+            GameSession.Instance.PlayerData.MaxFireflyLight = _maxFireflyLight;
+        }
+
+        private void Update()
+        {
+            if (!_saveZoneMask.IsTouchingLayer && _loseLightTimer.IsReady)
+            {
+                var newValue = _fireflyLight.Value - _lightLoseByTick;
+                _fireflyLight.Value = Mathf.Max(0, newValue);
+                _loseLightTimer.Reset();
+            }
+
+            if (_saveZoneMask.IsTouchingLayer && _regenLightTimer.IsReady)
+            {
+                var newValue = _fireflyLight.Value + _lightRegenByTick;
+                _fireflyLight.Value = Mathf.Min(newValue, _maxFireflyLight);
+                _regenLightTimer.Reset();
+            }
         }
     }
 }
--- Assets/Scripts/Player/SanityComponent.cs
+++ Assets/Scripts/Player/SanityComponent.cs
@@ -3,6 +3,7 @@ using Model;
 using PersistantData;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player
 {
@@ -11,7 +12,9 @@ namespace Player
         [SerializeField] private float _maxSanity;
         [SerializeField] private Timer _sanityTickLoseTimer;
         [SerializeField] private float _sanityLoseByTick;
-        [SerializeField] private LayerCheck _darknessCheck;
+        [SerializeField] private Timer _sanityTickRegenTimer;
+        [SerializeField] private float _sanityRegenByTick;
+        [SerializeField] private LayerCheck _saveZoneCheck;
         [SerializeField] private LayerCheck _lightCheck;
         private FloatProperty _sanity;
 
@@ -30,18 +33,18 @@ namespace Player
 
         private void Update()
         {
-            if (_darknessCheck.IsTouchingLayer && !_lightCheck.IsTouchingLayer && _sanityTickLoseTimer.IsReady)
+            if (!_lightCheck.IsTouchingLayer && !_saveZoneCheck.IsTouchingLayer && _sanityTickLoseTimer.IsReady)
             {
                 var newSanityValue = _sanity.Value - _sanityLoseByTick;
                 Sanity.Value = math.max(0, newSanityValue);
                 _sanityTickLoseTimer.Reset();
             }
 
-            if (_lightCheck.IsTouchingLayer && _sanityTickLoseTimer.IsReady)
+            if (_saveZoneCheck.IsTouchingLayer && _sanityTickRegenTimer.IsReady)
             {
-                var newSanityValue = _sanity.Value + _sanityLoseByTick;
+                var newSanityValue = _sanity.Value + _sanityRegenByTick;
                 _sanity.Value = Mathf.Min(newSanityValue, _maxSanity);
-                _sanityTickLoseTimer.Reset();
+                _sanityTickRegenTimer.Reset();
             }
         }
     }
