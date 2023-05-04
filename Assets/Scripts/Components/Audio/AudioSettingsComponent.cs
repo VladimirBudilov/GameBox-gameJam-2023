40c3d8667b1c653b949d5def755e38098c796853
Fenneig <ninetailedkot92@gmail.com>
2023-05-04 07:38:39 +0300

    Save audio settings

---
+++ Assets/Scripts/Components/Audio/AudioSettingsComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Model;
+using PersistantData;
+using UnityEngine;
+
+namespace Components.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSettingComponent : MonoBehaviour
+    {
+        [SerializeField] SoundSettings _mode;
+        private AudioSource _source;
+        private FloatPersistentProperty _model;
+
+        public AudioSource Source => _source;
+
+        private void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+            _model = FindProperty();
+            _model.OnChanged += OnSoundSettingsChange;
+
+            OnSoundSettingsChange(_model.Value, _model.Value);
+        }
+
+        private void OnSoundSettingsChange(float newValue, float oldValue)
+        {
+            _source.volume = newValue;
+        }
+
+        private FloatPersistentProperty FindProperty()
+        {
+            switch (_mode)
+            {
+                case SoundSettings.Music: return GameSettings.Instance.Music;
+                case SoundSettings.Sfx: return GameSettings.Instance.Sfx;
+            }
+
+            throw new ArgumentException("Undefiend argument");
+        }
+
+        private void OnDestroy()
+        {
+            _model.OnChanged -= OnSoundSettingsChange;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using UI.Widgets;
+using UnityEngine;
+
+namespace UI.Menus
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        [SerializeField] private AudioSettingsWidget _music;
+        [SerializeField] private AudioSettingsWidget _sfx;
+        protected void Start()
+        {
+            _music.SetModel(GameSettings.Instance.Music);
+            _sfx.SetModel(GameSettings.Instance.Sfx);
+        }
+    }
+}
