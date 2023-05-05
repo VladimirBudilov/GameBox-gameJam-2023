778791c3118d21da5b51f8a13c090d9a7a1428b9
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-05-05 18:39:31 +0400

    add diegetic light interface

--- Assets/Scripts/Components/GameplayObjects/Creatures/FireflyLightComponent.cs
+++ Assets/Scripts/Components/GameplayObjects/Creatures/FireflyLightComponent.cs
@@ -2,19 +2,26 @@ using Components.ColliderBased;
 using Model;
 using PersistantData;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using Utils;
 
 namespace Components.GameplayObjects.Creatures
 {
+    [RequireComponent(typeof(CircleCollider2D))]
     public class FireflyLightComponent : MonoBehaviour
     {
         [SerializeField] private float _maxFireflyLight;
+        [SerializeField] private float _maxFireflyLightModifer = 40f;
         [SerializeField] private Timer _loseLightTimer;
         [SerializeField] private float _lightLoseByTick;
         [SerializeField] private Timer _regenLightTimer;
         [SerializeField] private float _lightRegenByTick;
         [SerializeField] private LayerCheck _saveZoneMask;
+        [SerializeField] private float _lightToColliderModifier;
+        [SerializeField] private float _minLight= 1f;
         private FloatProperty _fireflyLight;
+        [SerializeField] private CircleCollider2D _circleCollider;
+        [SerializeField] private Light2D _light;
 
         public FloatProperty FireflyLight { get => _fireflyLight; set => _fireflyLight = value; }
         private void Start()
@@ -22,29 +29,55 @@ namespace Components.GameplayObjects.Creatures
             FireflyLight = GameSession.Instance.PlayerData.FireflyLight;
             FireflyLight.Value = _maxFireflyLight;
             GameSession.Instance.PlayerData.MaxFireflyLight = _maxFireflyLight;
+            _circleCollider.radius = GetRealRadius() - _lightToColliderModifier;
+            _light.pointLightOuterRadius = GetRealRadius();
         }
 
         private void Update()
         {
             if (!_saveZoneMask.IsTouchingLayer && _loseLightTimer.IsReady)
             {
-                var newValue = _fireflyLight.Value - _lightLoseByTick;
-                _fireflyLight.Value = Mathf.Max(0, newValue);
-                _loseLightTimer.Reset();
+                FireflyLoosingLight();
+                SetLightAndCollider();
             }
 
             if (_saveZoneMask.IsTouchingLayer && _regenLightTimer.IsReady)
             {
-                var newValue = _fireflyLight.Value + _lightRegenByTick;
-                _fireflyLight.Value = Mathf.Min(newValue, _maxFireflyLight);
-                _regenLightTimer.Reset();
+                FireflyRegeneratingLight();
+                SetLightAndCollider();
+                
             }
+        }
+
+        private void SetLightAndCollider()
+        {
+            _light.pointLightOuterRadius = GetRealRadius();
+            _circleCollider.radius = GetRealRadius() - _lightToColliderModifier;
+        }
+
+        private void FireflyRegeneratingLight()
+        {
+            var newValue = _fireflyLight.Value + _lightRegenByTick;
+            _fireflyLight.Value = Mathf.Min(newValue, _maxFireflyLight);
+            _regenLightTimer.Reset();
+        }
+
+        private void FireflyLoosingLight()
+        {
+            var newValue = _fireflyLight.Value - _lightLoseByTick;
+            _fireflyLight.Value = Mathf.Max(_minLight, newValue);
+            _loseLightTimer.Reset();
         }
 
         public void Damage(float amount)
         {
             var newValue = FireflyLight.Value - amount;
             FireflyLight.Value = Mathf.Max(0, newValue);
+        }
+
+        private float GetRealRadius()
+        {
+            return _fireflyLight.Value / _maxFireflyLightModifer;
         }
     }
 }
